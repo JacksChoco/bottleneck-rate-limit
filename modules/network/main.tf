9411d3fefbd9dc6dfde4c365f3b2c88441534d86
JacksChoco <ljs8860@playauto.co.kr>
2018-05-22 20:38:01 +0900

    alb 추가

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -23,6 +23,15 @@ module "security_group" {
   vpc_id      = "${module.vpc.id}"
 }
 
+module "alb" {
+  source = "../alb"
+
+  alb_name          = "${var.environment}_alb"
+  environment       = "${var.environment}"
+  vpc_id            = "${module.vpc.id}"
+  public_subnet_ids = "${module.public_subnet.ids}"
+}
+
 resource "aws_route" "public_igw_route" {
   count                  = "${length(var.public_subnet_cidrs)}"
   route_table_id         = "${element(module.public_subnet.route_table_ids, count.index)}"
