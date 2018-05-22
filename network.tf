1e1928c76e247f472ff2f617c69f325dce8f5f0d
JacksChoco <ljs8860@playauto.co.kr>
2018-05-22 20:13:20 +0900

    state 리모트로 관리하기

--- network.tf
+++ network.tf
@@ -12,6 +12,61 @@ module "network" {
   depends_id           = ""
 }
 
+### 백업용 S3 및 dynamodb
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "TerraformStateLock"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+// 로그 저장용 버킷
+resource "aws_s3_bucket" "logs" {
+  bucket = "kr.ne.outsider.logs"
+  acl    = "log-delivery-write"
+}
+
+// Terraform state 저장용 S3 버킷
+resource "aws_s3_bucket" "terraform-state" {
+  bucket = "kr.ne.outsider.terraform.state"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  tags {
+    Name = "terraform state"
+  }
+
+  logging {
+    target_bucket = "${aws_s3_bucket.logs.id}"
+    target_prefix = "log/"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+terraform {
+  required_version = ">= 0.9.5"
+
+  backend "s3" {
+    bucket     = "kr.ne.outsider.terraform.state"
+    key        = "test/terraform.tfstate"
+    region     = "ap-northeast-2"
+    encrypt    = true
+    lock_table = "TerraformStateLock"
+    acl        = "bucket-owner-full-control"
+  }
+}
+
 variable "vpc_cidr" {}
 variable "environment" {}
 variable "max_size" {}
@@ -30,4 +85,16 @@ variable "public_subnet_cidrs" {
 
 variable "availability_zones" {
   type = "list"
+}
+
+output "vpc_id" {
+  value = "${module.network.vpc_id}"
+}
+
+output "vpc_cidr" {
+  value = "${module.network.vpc_cidr}"
+}
+
+output "public_subnet_ids" {
+  value = "${module.network.public_subnet_ids}"
 }
