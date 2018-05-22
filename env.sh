1a3b8f94455372c2123e000703c8fbac26adbd7a
JacksChoco <ljs8860@playauto.co.kr>
2018-05-22 20:58:05 +0900

    세팅

--- env.sh
+++ env.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 export AWS_ACCESS_KEY_ID=`aws configure get default.aws_access_key_id`
 export AWS_SECRET_ACCESS_KEY=`aws configure get default.aws_secret_access_key`
-export SECURITY_GROUP="sg-eb78cc81"
-export VPC="vpc-96b8a9ff"
-export SUBNET="subnet-3bad4053"
+export SECURITY_GROUP="terraform output security_group_id"
+export VPC=`terraform output vpc_id`
+export SUBNET=${$(terraform output public_subnet_ids)[@]} # => array문을 join 시킬 수 있음 [1,2,3] => 1,2,3
 export REGION="ap-northeast-2"
-export INSTANCE_TYPE="t2.medium"
+export INSTANCE_TYPE="t2.micro"
 export KEY_PAIR="docker"
-export TARGET_GROUP_ARN="arn:aws:elasticloadbalancing:ap-northeast-2:458834177603:targetgroup/hello-service/d234a8f93453568d"
-export ECR_HOST="458834177603.dkr.ecr.ap-northeast-2.amazonaws.com"
+export TARGET_GROUP_ARN=`terraform output default_alb_target_group`
