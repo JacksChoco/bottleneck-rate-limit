93f2ba75480ee7e7ad367f81fd7d5e156a5f9fb3
JacksChoco <ljs8860@playauto.co.kr>
2018-05-22 19:39:30 +0900

    nat 제거

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -5,16 +5,6 @@ module "vpc" {
   environment = "${var.environment}"
 }
 
-module "private_subnet" {
-  source = "../subnet"
-
-  name               = "${var.environment}_private_subnet"
-  environment        = "${var.environment}"
-  vpc_id             = "${module.vpc.id}"
-  cidrs              = "${var.private_subnet_cidrs}"
-  availability_zones = "${var.availability_zones}"
-}
-
 module "public_subnet" {
   source = "../subnet"
 
@@ -25,13 +15,6 @@ module "public_subnet" {
   availability_zones = "${var.availability_zones}"
 }
 
-module "nat" {
-  source = "../nat_gateway"
-
-  subnet_ids   = "${module.public_subnet.ids}"
-  subnet_count = "${length(var.public_subnet_cidrs)}"
-}
-
 resource "aws_route" "public_igw_route" {
   count                  = "${length(var.public_subnet_cidrs)}"
   route_table_id         = "${element(module.public_subnet.route_table_ids, count.index)}"
@@ -39,12 +22,29 @@ resource "aws_route" "public_igw_route" {
   destination_cidr_block = "${var.destination_cidr_block}"
 }
 
-resource "aws_route" "private_nat_route" {
-  count                  = "${length(var.private_subnet_cidrs)}"
-  route_table_id         = "${element(module.private_subnet.route_table_ids, count.index)}"
-  nat_gateway_id         = "${element(module.nat.ids, count.index)}"
-  destination_cidr_block = "${var.destination_cidr_block}"
-}
+# module "private_subnet" {
+#   source = "../subnet"
+
+#   name               = "${var.environment}_private_subnet"
+#   environment        = "${var.environment}"
+#   vpc_id             = "${module.vpc.id}"
+#   cidrs              = "${var.private_subnet_cidrs}"
+#   availability_zones = "${var.availability_zones}"
+# }
+
+# module "nat" {
+#   source = "../nat_gateway"
+
+#   subnet_ids   = "${module.public_subnet.ids}"
+#   subnet_count = "${length(var.public_subnet_cidrs)}"
+# }
+
+# resource "aws_route" "private_nat_route" {
+#   count                  = "${length(var.private_subnet_cidrs)}"
+#   route_table_id         = "${element(module.private_subnet.route_table_ids, count.index)}"
+#   nat_gateway_id         = "${element(module.nat.ids, count.index)}"
+#   destination_cidr_block = "${var.destination_cidr_block}"
+# }
 
 # Creating a NAT Gateway takes some time. Some services need the internet (NAT Gateway) before proceeding.
 # Therefore we need a way to depend on the NAT Gateway in Terraform and wait until is finished.
