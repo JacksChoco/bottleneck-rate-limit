f77612519baae32efadfcd90bac98daefa537869
JacksChoco <ljs8860@playauto.co.kr>
2018-05-22 21:22:11 +0900

    프로젝트 이름 세팅 환경변수로 바꿈

--- env.sh
+++ env.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+export PROJECT_NAME="bottleneck"
+export TF_VAR_environment="bottleneck"
 export AWS_ACCESS_KEY_ID=`aws configure get default.aws_access_key_id`
 export AWS_SECRET_ACCESS_KEY=`aws configure get default.aws_secret_access_key`
 export SECURITY_GROUP=`terraform output security_group_id`
@@ -8,4 +10,3 @@ export REGION="ap-northeast-2"
 export INSTANCE_TYPE="t2.micro"
 export KEY_PAIR="docker"
 export TARGET_GROUP_ARN=`terraform output default_alb_target_group`
-export PROJECT_NAME="bottleneck"
