61f163b6062435cd5ed8544c1415f098965e9250
JacksChoco <ljs8860@playauto.co.kr>
2018-05-24 11:08:54 +0900

    스크립트 정리

--- ecs_create.sh
+++ ecs_create.sh
@@ -12,3 +12,19 @@ ecs-cli up --keypair ${KEY_PAIR} \
   --security-group ${SECURITY_GROUP} --cluster ${PROJECT_NAME} \
   --vpc ${VPC} --subnets ${SUBNET} \
   --capability-iam --size 2 --instance-type ${INSTANCE_TYPE}
+
+ecs-cli compose --file deploy-compose.yml \
+  --project-name ${PROJECT_NAME} \
+  service create --cluster ${PROJECT_NAME} \
+  --deployment-max-percent 200 \
+  --deployment-min-healthy-percent 50 \
+  --target-group-arn ${TARGET_GROUP_ARN} \
+  --health-check-grace-period 30 \
+  --container-name ${PROJECT_NAME} \
+  --container-port 3000 \
+  --create-log-groups
+
+  ecs-cli compose --file deploy-compose.yml \
+  --project-name ${PROJECT_NAME} \
+  service up \
+  --cluster ${PROJECT_NAME}
